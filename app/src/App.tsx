c9ce2ff0d5654f86634b9c3e486684748cfd0762
Dean Geckt <t-deangeckt@microsoft.com>
2021-03-26 14:09:35 +0300

    folder structure + router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
+import { useHistory } from "react-router-dom";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	const history = useHistory();
+	return (
+		<div className="App">
+			<Button className="Button" variant="outlined" color="primary" onClick={() => history.push( {pathname: '/design'} )}>
+				New Neuron
+			</Button>
+
+			<Button className="Button" variant="outlined" color="primary" onClick={() => console.log('fgsf')}>
+				Import Neuron
+			</Button>
+		</div>
+
   );
 }
 
---
+++ app/src/design/Design.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Design.css';
+
+
+function Design() {
+  return (
+    <div className="Design">
+        design page
+    </div>
+
+  );
+}
+
+export default Design;
