d66ade2d53c8093732a477ea07bb142307b9b015
Dean Geckt <t-deangeckt@microsoft.com>
2021-03-27 13:43:00 +0300

    control the plot a bit

--- app/src/simulate/Plot.tsx
+++ app/src/simulate/Plot.tsx
@@ -1,50 +1,16 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+function randomData() {
+    var data = []
+    for (var i = 0; i < 50; i++) {
+        var a = Math.floor(Math.random() * 10);
+        var b = Math.floor(Math.random() * 10);
+
+        data.push({'time': i, "A": a, "B": b})
+    }
+    return data;
+}
 
 function Plot() {
 
@@ -53,7 +19,7 @@ function Plot() {
         <LineChart
           width={500}
           height={300}
-          data={data}
+          data={randomData()}
           margin={{
             top: 5,
             right: 30,
@@ -62,12 +28,15 @@ function Plot() {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <XAxis dataKey="time">
+            <Label value="Time [mS]" offset={0} position="insideBottom" />
+          </XAxis>
+          <YAxis label={{ value: 'Voltage [mV]', angle: -90, position: 'insideLeft' }}/>
           <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Legend verticalAlign="top" height={0}/>
+          <Line type="basis" dataKey="A" stroke="#8884d8" dot={false}/>
+          <Line type="basis" dataKey="B" stroke="#228B22" dot={false}/>
+
         </LineChart>
       </ResponsiveContainer>
     );
