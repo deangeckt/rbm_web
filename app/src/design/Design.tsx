2d5309c7c285e74f6c642eaf4b9005583ba90937
Dean Geckt <t-deangeckt@microsoft.com>
2021-03-29 15:34:47 +0300

    design - remove canvas drawing - using knova d&d

--- app/src/design/Design.tsx
+++ app/src/design/Design.tsx
@@ -1,115 +1,99 @@
-import React, { useRef, useEffect, useState  } from 'react'
-import './Design.css';
-export interface IPoint {
-	x: number;
-	y: number;
+import React from 'react';
+import { Stage, Layer, Line, Transformer } from 'react-konva';
+
+interface ILine {
+	id: string;
+	points: number[];
+	rotation: number;
 }
 
+interface TransLineProps {
+	shapeProps: any;
+	isSelected: boolean;
+	onSelect: Function;
+}
 
-const pointsInit: IPoint[] = [];
+const INITIAL_STATE: ILine[] = [
+	{
+		points: [10,50, 10,100],
+		id: '0',
+		rotation: 0
+	},
+	{
+		points: [100,50, 100,100],
+		id: '1',
+		rotation: 0
+	},
+]
+
+const TransLine = ({ shapeProps: shareProps, isSelected, onSelect }: TransLineProps) => {
+	const shapeRef = React.useRef();
+	const trRef = React.useRef<any>();
+
+	React.useEffect(() => {
+		if (isSelected) {
+			trRef.current!.nodes([shapeRef.current]);
+			trRef.current!.getLayer().batchDraw();
+		}
+	}, [isSelected]);
+
+	return (
+		<React.Fragment>
+			<Line
+				onClick={onSelect}
+				onTap={onSelect}
+				ref={shapeRef}
+				stroke={'red'}
+				strokeWidth={5}
+				{...shareProps}
+				draggable
+			/>
+			{isSelected && (
+        		<Transformer ref={trRef} />
+      		)}
+    </React.Fragment>
+  );
+};
 
 
 const Design = () => {
-  	const canvasRef = useRef(null);
-	const [isDrawing, setIsDrawing] = useState(false);
-	const [isMoving, setIsMoving] = useState(false);
-	const [points, setPoints] = useState(pointsInit);
-	const [size, setSize] = useState(0);
+	const [lines, setLines] = React.useState(INITIAL_STATE);
+	const [selectedId, setSelectedId] = React.useState('');
 
-	const addPoint = (x: number, y: number) => {
-		const currPoints = [...points];
-		currPoints.push({x:x, y:y});
-		setPoints(currPoints);
-		setSize(size + 1);
-	}
-
-	const swapLastPoint = (x: number, y: number) => {
-		const currPoints = [...points];
-		currPoints[size] = {x:x, y:y};
-		setPoints(currPoints);
-	}
-
-	const removeLastPoint = () => {
-		const currPoints = [...points];
-		currPoints.splice(size, 1);
-		setPoints(currPoints);
-	}
-
-	const draw = () => {
-		const canvas = canvasRef.current as any;
-		var ctx = canvas.getContext("2d");
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		for (let i = 0; i < points.length; i++) {
-			if (i % 2 === 0) {
-				ctx.beginPath();
-				// ctx.lineCap = 'round'
-				ctx.moveTo(points[i].x, points[i].y);
-			} else {
-				ctx.lineTo(points[i].x, points[i].y);
-				ctx.stroke()
-			}
-		};
-	}
-
-	const getCoords = (e: any) => {
-		const canvas = canvasRef.current as any;
-		const canvasPosition = canvas.getBoundingClientRect()
-		const x =  e.clientX - canvasPosition.left;
-		const y =  e.clientY - canvasPosition.top;
-		return [x, y];
-	}
-
-	const onMouseDown = (e: any) => {
-		const [x, y] = getCoords(e);
-		addPoint(x,y);
-		setIsDrawing(true);
-	}
-
-	const onMouseMove = (e: any) => {
-		if (!isDrawing)
-			return;
-
-		setIsMoving(true);
-		const [x, y] = getCoords(e);
-		swapLastPoint(x,y);
-		draw();
-	}
-
-	const onMouseUp = (_e: any) => {
-		setIsDrawing(false);
-		if (isMoving) {
-			setIsMoving(false);
-		} else {
-			removeLastPoint();
+	const checkDeselect = (e: any) => {
+		// deselect when clicked on empty area
+		const clickedOnEmpty = e.target === e.target.getStage();
+		if (clickedOnEmpty) {
+			setSelectedId('');
+			console.log(lines);
 		}
-		console.log(points);
-	}
+	};
 
+	return (
+	<Stage width={window.innerWidth} height={window.innerHeight}
+		   onMouseDown={checkDeselect} onTouchStart={checkDeselect}>
+		<Layer>
+			<TransLine
+				key={'0'}
+				shapeProps={INITIAL_STATE}
+				isSelected={'0' === selectedId}
+				onSelect={() => {
+					setSelectedId('0');
+				}}
+			/>
 
-	useEffect(() => {
-		console.log('init')
-		const canvas = canvasRef.current as any;
-		const ctx = canvas.getContext('2d');
-
-		canvas.height = window.innerHeight - 100
-		canvas.width = window.innerWidth - 100
-
-		ctx.lineWidth = 2;
-		ctx.strokeStyle = 'red';
-
-	}, [])
-
-  return (
-		<div className='Design'>
-			<div className="Container">
-				<canvas ref={canvasRef} className="Canvas" onMouseDown={onMouseDown}
-				onMouseMove={onMouseMove} onMouseUp={onMouseUp} />
-			</div>
-		</div>
+			{lines.map((l) => (
+				<TransLine
+					key={l.id}
+					shapeProps={l}
+					isSelected={l.id === selectedId}
+					onSelect={() => {
+						setSelectedId(l.id);
+					}}
+				/>
+			))}
+		</Layer>
+	</Stage>
   );
-}
-
-export default Design
-
-
+};
+export default Design;
