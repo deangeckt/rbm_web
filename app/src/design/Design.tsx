d9ea1db178e05aa11ac0e284662c014c4b97095c
Dean Geckt <t-deangeckt@microsoft.com>
2021-03-30 14:42:15 +0300

    design - impl all conrtol panel functions

--- app/src/design/Design.tsx
+++ app/src/design/Design.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Stage, Layer, Circle } from 'react-konva';
 import TransformerLine from './TransformerLine';
-import { Button, TextField } from '@material-ui/core';
+import { Button, InputAdornment, MenuItem, TextField } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 interface IRenderLine {
 	id: number;
@@ -9,13 +11,36 @@ interface IRenderLine {
 	points: number[]; // [x1,y1, x2,y2]
 	radius: number
 	pid: number;
+	length: number;
+	alpha: number;
 }
 
+const types = [
+	{
+	  value: 0,
+	  label: 'undefined',
+	},
+	{
+	  value: '2',
+	  label: 'axon',
+	},
+	{
+	  value: '3',
+	  label: 'basal dendrite',
+	},
+	{
+	  value: '4',
+	  label: 'apical dendrite',
+	},
+];
+
 const default_radius = 0.1;
+const default_tid = 0;
+const default_length = 100;
+const default_alpha = Math.PI * 0.1;
 const none_selected = -1;
 const root_id = 1;
 const [rootX, rootY] = [window.innerWidth / 2, window.innerHeight / 2 + 50];
-
 const init_render_lines: IRenderLine[] = [];
 
 const Design = () => {
@@ -29,10 +54,6 @@ const Design = () => {
 			console.log(renderLines);
 		}
 	};
-
-	const distanceAlphaToXy = (d: number, alpha: number, prevX: number, prevY: number) => {
-		return [prevX + d * Math.cos(alpha), (prevY - d * Math.sin(alpha))];
-	}
 
 	const getLineChildren = (selectedLine: IRenderLine) => {
 		const lines = [...renderLines];
@@ -49,11 +70,11 @@ const Design = () => {
 		newPoints.push(prevX);
 		newPoints.push(prevY);
 
-		const newAngle = (childrenCound + 1) * Math.PI * 0.1;
-		const [newX, newY] = distanceAlphaToXy(100, newAngle, prevX, prevY);
+		const newAngle = (childrenCound + 1) * default_alpha;
+		const [newX, newY] = lengthAlphaToXy(default_length, newAngle, prevX, prevY);
 		newPoints.push(newX);
 		newPoints.push(newY);
-		return newPoints;
+		return {newPoints, newAngle};
 	}
 
 	const addNew = () => {
@@ -62,20 +83,25 @@ const Design = () => {
 		let newId: number;
 		let newPid: number;
 		let newPoints: number[] = [];
+		let newAlpha: number;
 
 		if (!selectedLine) {
 			const rootChilds = getRootChildren().length;
+			const r = addNewPoints(rootX, rootY, rootChilds);
+			newPoints = r.newPoints;
+			newAlpha = r.newAngle;
 			newPid = root_id;
-			newPoints = addNewPoints(rootX, rootY, rootChilds);
 			newId = rootChilds === 0 ? root_id + 1 :lines[lines.length - 1].id + 1;
 		} else {
 			const prevX = selectedLine.points[2];
 			const prevY = selectedLine.points[3];
-			newPoints = addNewPoints(prevX, prevY, getLineChildren(selectedLine).length);
+			const r = addNewPoints(prevX, prevY, getLineChildren(selectedLine).length);
+			newPoints = r.newPoints;
+			newAlpha = r.newAngle;
 			newId = lines[lines.length - 1].id + 1;
 			newPid = selectedLine.id;
 		}
-		lines.push({id: newId, points: newPoints, tid: -1, pid: newPid, radius: default_radius});
+		lines.push({id: newId, points: newPoints, pid: newPid, radius: default_radius, tid: default_tid, length: default_length, alpha: newAlpha});
 		setRenderLines(lines);
 		setSelectedId(newId);
 	}
@@ -86,38 +112,143 @@ const Design = () => {
 		return selectedLine ? selectedLine.radius : default_radius;
 	}
 
+	const getSelectedType = () => {
+		const lines = [...renderLines];
+		const selectedLine = lines.find((line) => line.id === selectedId);
+		return selectedLine ? selectedLine.tid : default_tid;
+	}
+
 	const updateSimpleField = (field: "tid" | "radius", value: number) => {
 		const lines = [...renderLines];
 		const selectedLine = lines.find((line) => line.id === selectedId);
-
-		if (!selectedLine)
-			return;
-
 		(selectedLine as any)[field] = value;
 		setRenderLines(lines);
 	}
 
-	// TODO panel to control radius ~ strokeWidth, tid -> enum, and distance and alpha to control ending of point
+	const lengthAlphaToXy = (d: number, alpha: number, prevX: number, prevY: number) => {
+		return [prevX + d * Math.cos(alpha), (prevY - d * Math.sin(alpha))];
+	}
+
+	const updateLinePoint = (line: IRenderLine) =>{
+		const prevX = line.points[0];
+		const prevY = line.points[1];
+		const [newX, newY] = lengthAlphaToXy(line.length, line.alpha, prevX, prevY);
+		line.points[2] = newX;
+		line.points[3] = newY;
+	}
+
+	const getSelectedAlpha = () => {
+		const lines = [...renderLines];
+		const selectedLine = lines.find((line) => line.id === selectedId);
+		const alpha = selectedLine ? selectedLine.alpha : default_alpha;
+		return (alpha / Math.PI);
+	}
+
+	const getSelectedLength = () => {
+		const lines = [...renderLines];
+		const selectedLine = lines.find((line) => line.id === selectedId);
+		return selectedLine ? selectedLine.length : default_length;
+	}
+
+	const updateChildsRecur = (father: IRenderLine): void => {
+		const childs = getLineChildren(father);
+		childs.forEach(child => {
+			child.points[0] = father.points[2];
+			child.points[1] = father.points[3];
+			updateLinePoint(child);
+			updateChildsRecur(child);
+		});
+	}
+
+	const deleteChildsRecur = (lines: IRenderLine[], fatherIdx: number): void => {
+		const childs = lines.filter((line) => line.pid === lines[fatherIdx].id); //use map to return idx?
+		console.log('childs', childs);
+		childs.forEach(child => {
+			const childIdx = lines.findIndex((line) => line.id === child.id);
+			deleteChildsRecur(lines, childIdx);
+		});
+		lines.splice(fatherIdx, 1)
+	}
+
+	const updateAlpha = (value: number) => {
+		const lines = [...renderLines];
+		const selectedLine = lines.find((line) => line.id === selectedId);
+		if (!selectedLine)
+			return;
+
+		selectedLine.alpha = value * Math.PI;
+		updateLinePoint(selectedLine);
+		updateChildsRecur(selectedLine);
+		setRenderLines(lines);
+	}
+
+	const updateLength = (value: number) => {
+		const lines = [...renderLines];
+		const selectedLine = lines.find((line) => line.id === selectedId);
+		if (!selectedLine)
+			return;
+
+		selectedLine.length = value;
+		updateLinePoint(selectedLine);
+		updateChildsRecur(selectedLine);
+		setRenderLines(lines);
+	}
+
+	const Delete = () => {
+		const lines = [...renderLines];
+		const index = lines.findIndex((line) => line.id === selectedId);
+		if (index === -1)
+			return;
+
+		deleteChildsRecur(lines, index);
+		setRenderLines(lines);
+	}
+
+	// TODO add all text fields to control panel component
 	// TODO radius validation > 0 - maybe show err
+	// TODO - new line - make sure alpha is spare
+	// TODO at export / finish -> fix spaces in ID's due to deletes - recur fix
 	return (
 	<>
 	<div>
-		<Button className="Button" variant="outlined" color="primary" disabled={selectedId === -1} onClick={() => addNew()}>
-			Add
+		<Button variant="outlined" color="primary" startIcon={<AddIcon />}
+				disabled={selectedId === none_selected} onClick={() => addNew()}>
+			Add Line
 		</Button>
+		<TextField label={'Length [µM]'} variant="filled" type="number" value={getSelectedLength()}
+					disabled={selectedId === none_selected || selectedId === root_id}
+					onChange={(e) => updateLength(Number(e.target.value))} />
+		<TextField label={'α [Rad]'} variant="filled" type="number" value={getSelectedAlpha()}
+					disabled={selectedId === none_selected || selectedId === root_id}
+					onChange={(e) => updateAlpha(Number(e.target.value))}
+					InputProps={{	inputProps: { min: 0, max: 2 * Math.PI, step: default_alpha * 0.1,  },
+									endAdornment: <InputAdornment position="end">PI</InputAdornment>, }} />
 		<TextField label={'Radius [µM]'} variant="filled" type="number" value={getSelectedRadius()}
+					disabled={selectedId === none_selected || selectedId === root_id}
 					onChange={(e) => updateSimpleField('radius', Number(e.target.value))} />
-
+		<TextField select label="Type" variant="filled"  value={getSelectedType()}
+					onChange={(e) => updateSimpleField('tid', Number(e.target.value))}
+					disabled={selectedId === none_selected || selectedId === root_id} >
+        	{types.map((option) => (
+				<MenuItem key={option.value} value={option.value}>
+					{option.label}
+				</MenuItem>
+        	))}
+        </TextField>
+		<Button variant="outlined" color="primary" startIcon={<DeleteIcon />}
+				disabled={selectedId === none_selected || selectedId === root_id} onClick={() => Delete()}>
+			Delete Line
+		</Button>
 	</div>
 	<Stage width={window.innerWidth} height={window.innerHeight}
 		   onMouseDown={checkDeselect} onTouchStart={checkDeselect}>
 		<Layer>
 			<Circle
-				radius={20}
-				fill={'red'}
+				radius={10}
+				fill={'black'}
 				opacity={selectedId === root_id ? 0.8 : 0.3}
 				x={rootX}
-				y={rootY + 20}
+				y={rootY}
 				draggable={false}
 				onClick={() => setSelectedId(root_id)}
 			/>
