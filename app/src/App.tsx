bf0696ea5af685b2a7be3d08d6dbf511330bb410
Nir Tzezana <nitzezan@microsoft.com>
2021-04-03 22:44:04 +0300

    Addition of ESLint and more

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,48 +1,52 @@
 import React, { useContext } from 'react';
-import { Button} from '@material-ui/core';
-import { useHistory } from "react-router-dom";
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import { importFile } from './Utils/SwcUtils';
 import './App.css';
 import { AppContext } from './Contexts/AppContext';
 import { init_app_state } from './Wrapper';
 
-function App() {
-	const history = useHistory();
-	const {state, setState} = useContext(AppContext);
+function App(): JSX.Element {
+    const history = useHistory();
+    const { state, setState } = useContext(AppContext);
 
-	const uploadFile = async (e: any) => {
-		e.preventDefault()
-		const reader = new FileReader()
-		reader.onload = async (e) => {
-			const text = e?.target?.result;
-			if (text) {
-				const r = importFile(text as string, state.stage.rootX, state.stage.rootY);
-				setState({...state, r});
-				history.push({pathname: '/design'});
-			}
-		};
-		reader.readAsText(e?.target?.files[0])
-	}
+    const uploadFile = async (e: any) => {
+        e.preventDefault();
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const text = e?.target?.result;
+            if (text) {
+                const r = importFile(text as string, state.stage.rootX, state.stage.rootY);
+                setState({ ...state, r });
+                history.push({ pathname: '/design' });
+            }
+        };
+        reader.readAsText(e?.target?.files[0]);
+    };
 
-	return (
-		<div className="App">
-				<div className="Container">
-				<Button className="Button" variant="outlined" color="primary"
-						onClick={() => {
-							// TODO: bug -> from design getting back here & clicking new neuron doesnt reset state
-							setState({...state, init_app_state});
-							history.push({pathname: '/design'})
-						}}>
-					New Neuron
-				</Button>
-				<div className="Divider"/>
-				<Button className="Button" variant="outlined" color="primary" component="label" >
-					Import Neuron
-					<input type="file" accept={".txt, .swc"} hidden onChange={(e) => uploadFile(e)}/>
-				</Button>
-			</div>
-		</div>
-  );
+    return (
+        <div className="App">
+            <div className="Container">
+                <Button
+                    className="Button"
+                    variant="outlined"
+                    color="primary"
+                    onClick={() => {
+                        // TODO: bug -> from design getting back here & clicking new neuron doesnt reset state
+                        setState({ ...state, init_app_state });
+                        history.push({ pathname: '/design' });
+                    }}
+                >
+                    New Neuron
+                </Button>
+                <div className="Divider" />
+                <Button className="Button" variant="outlined" color="primary" component="label">
+                    Import Neuron
+                    <input type="file" accept={'.txt, .swc'} hidden onChange={(e) => uploadFile(e)} />
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- app/src/api/api.ts
+++ app/src/api/api.ts
@@ -1,18 +1,20 @@
-import axios, {AxiosError, AxiosResponse} from 'axios';
+import axios, {AxiosResponse} from 'axios';
 
-export const run = (setData: Function): void => {
-    axios.request({
-        url: 'http://localhost:8080/api/v1/run',
-        method: 'POST',
-        data: {bla: 'bla'}
-    }).then((response: AxiosResponse) => {
-            const t = response.data['time'] as number[];
-            const v = response.data['volt'] as number[];
-            const r = [];
-            for (var i = 0 ; i<t.length; i++)
-                r.push([t[i], v[i]]);
-            setData(r);
-    }).catch((error: AxiosError) => {
-            console.error('err');
-    });
+export const run = async (setData: Function) => {
+    try {
+        const response = await axios.request({
+            url: 'http://localhost:8080/api/v1/run',
+            method: 'POST',
+            data: {bla: 'bla'}
+        }) as AxiosResponse;
+        const t = response.data['time'] as number[];
+        const v = response.data['volt'] as number[];
+        const r = [];
+        for (var i = 0 ; i<t.length; i++)
+            r.push([t[i], v[i]]);
+        setData(r);  
+    }
+    catch (error: any) {
+        console.error('error');   
+    }
 }
