087784d157dd36ead10d836fb2103ff21baa2516
Dean Geckt <t-deangeckt@microsoft.com>
2021-04-12 16:55:18 +0300

    server - support multiple recording and schema

--- server/neuronWrapper.py
+++ server/neuronWrapper.py
@@ -13,10 +13,27 @@ def num_to_type(type_):
         return h.axon
     elif type_ == 3:
         return h.dend
+    elif type_ == 0:
+        return h.dend_0
     elif type_ == 4:
         return h.apic
     else:
         raise ValueError('SWC - invalid type: ', type_)
+
+
+def recording_type_to_ref(type_, h_ref):
+    if type_ == 'volt':
+        return h_ref._ref_v
+    elif type_ == 'ina':
+        return h_ref._ref_ina
+    elif type_ == 'ik':
+        return h_ref._ref_ik
+    else:
+        raise ValueError('invalid record type: ', type_)
+
+
+def recording_key(recording_type_, type_, id_, section_):
+    return '{}_{}_{}_{}'.format(recording_type_, type_, id_, section_)
 
 
 class NeuronWrapper:
@@ -72,6 +89,27 @@ class NeuronWrapper:
             stims.append(new_stim)
         return stims
 
+    def add_record(self):
+        if 'record' not in self.config:
+            return
+        recordings = {}
+        for record_ in self.config['record']:
+            id_ = record_['id']
+            section_ = record_['section']
+            type_ = record_['type']
+            recording_type_ = record_['record_type']
+            recording_key_ = recording_key(recording_type_, type_, id_, section_)
+
+            h_ref = num_to_type(type_)
+            h_ref = h_ref[id_](section_)
+            h_ref = recording_type_to_ref(type_=recording_type_, h_ref=h_ref)
+
+            vrec = h.Vector()
+            vrec.record(h_ref)
+
+            recordings[recording_key_] = vrec
+        return recordings
+
     def run(self, params):
         self.init_params(params)
         self.init_swc()
@@ -79,30 +117,51 @@ class NeuronWrapper:
         h.celsius = self.config['celsius']
 
         h.soma[0].insert('hh')  # insert a Hodgkin-Huxley channel
-        stims = self.add_stim()
+        # h.dend_0[8].insert('hh')
 
-        vrec = h.Vector()  # setup recording Vectors
+        stims = self.add_stim()
+        recordings = self.add_record()
+
         trec = h.Vector()
-        vrec.record(h.soma[0](0.5)._ref_v)  # record voltage from middle (0.5) of the Section
         trec.record(h._ref_t)  # record time variable
 
         h.finitialize(self.config['rest_volt'])
         h.dt = self.config['dt']
         neuron.run(self.config['sim_time'])
-        return {'time': np.array(trec).tolist(), 'volt': np.array(vrec).tolist()}
+
+        result = {'time': np.array(trec).tolist()}
+        for record_key in recordings:
+            result[record_key] = np.array(recordings[record_key]).tolist()
+
+        return result
 
 
 if __name__ == "__main__":
     wrap = NeuronWrapper()
-    stim = [{'id': 0, 'section': 0.5, 'type': 1, 'delay': 20,
-             'duration': 25, 'amplitude': 5}]
+    stim = [{'id': 0, 'section': 0.5, 'type': 0, 'delay': 20, 'duration': 25, 'amplitude': 0.5},
+            ]
+    record = [{'id': 0, 'section': 0.5, 'type': 1, 'record_type': 'volt'},
+              {'id': 0, 'section': 0.5, 'type': 0, 'record_type': 'volt'}
+            ]
     res = wrap.run([{'id': 'swc_path', 'value': 'C:/Users/t-deangeckt/Downloads/swcTree.swc'},
-                    {'id': 'sim_time', 'value': 150},
+                    {'id': 'sim_time', 'value': 200},
                     {'id': 'rest_volt', 'value': -65},
-                    {'id': 'stim', 'value': stim}])
+                    {'id': 'stim', 'value': stim},
+                    {'id': 'record', 'value': record}])
 
-    plot(np.array(res['time']), np.array(res['volt']))
+    subplot(1, 2, 1)
+    r_key1 = recording_key('volt', 1, 0, 0.5)
+    r_key2 = recording_key('volt', 0, 0, 0.5)
+
+    plot(np.array(res['time']), np.array(res[r_key1]))
+    xlim((0, h.tstop))
+    ylabel(r_key1)
+    xlabel('Time (ms)')
+    tight_layout()
+
+    subplot(1, 2, 2)
+    plot(np.array(res['time']), np.array(res[r_key2]))
     xlim((0, h.tstop))
     xlabel('Time (ms)')
-    ylabel('v(0.5)')
+    ylabel(r_key2)
     show()
