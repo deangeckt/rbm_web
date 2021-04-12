c0043ca5640e8986a5e461840f1185c340224caf
Dean Geckt <t-deangeckt@microsoft.com>
2021-04-12 19:57:44 +0300

    server - load dynamically neuron moudle based on working dir

--- server/app.py
+++ server/app.py
@@ -1,27 +1,32 @@
 import json
-import os
 import sys
 import traceback
 from flask import Flask
 from flask import request
 from flask_cors import CORS
-
+import os
 from neuronWrapper import NeuronWrapper
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-wrapper = NeuronWrapper()
-
 
 @app.route('/api/v1/run', methods=['POST'])
 def score():
     try:
+        config_path = '../app/src/share/config.json'
+        config_full_path = os.path.join(os.getcwd(), config_path)
+
         data = request.get_json()
         for d in data:
             if 'id' not in d or 'value' not in d:
                 msg = "Data params - bad format"
                 raise ValueError(msg)
+            if 'neuron_path' in d:
+                print('Changing dir to: ', d['value'])
+                os.chdir(d['value'])
+
+        wrapper = NeuronWrapper(config_full_path)
         res = wrapper.run(data)
         return json.dumps(res), 200, {'Content-Type': 'application/json'}
 
--- server/neuronWrapper.py
+++ server/neuronWrapper.py
@@ -1,12 +1,8 @@
 import json
-import neuron
-from neuron import h
-from copy import deepcopy
-import numpy as np
 from pylab import *
 
 
-def num_to_type(type_):
+def num_to_type(type_, h):
     if type_ == 1:
         return h.soma
     elif type_ == 2:
@@ -37,23 +33,20 @@ def recording_key(recording_type_, type_, id_, section_):
 
 
 class NeuronWrapper:
-    def __init__(self):
-        h.load_file('stdrun.hoc')
-        h.load_file('import3d.hoc')
+    def __init__(self, config_path):
+        self.neuron_dy = __import__("neuron")
+        self.h = getattr(self.neuron_dy, "h")
 
-        self.default_config = {}
-        with open('../app/src/share/config.json') as f_:
+        self.h.load_file('stdrun.hoc')
+        self.h.load_file('import3d.hoc')
+
+        self.config = {}
+        with open(config_path) as f_:
             default_form = json.load(f_)['default_form']
             for form in default_form:
-                self.default_config[form['id']] = form['value']
-        self.config = None
-        self.reset_config()
-
-    def reset_config(self):
-        self.config = deepcopy(self.default_config)
+                self.config[form['id']] = form['value']
 
     def init_params(self, params):
-        self.reset_config()
         for tup in params:
             id_ = tup['id']
             val_ = tup['value']
@@ -63,13 +56,13 @@ class NeuronWrapper:
         if 'swc_path' not in self.config:
             raise ValueError('Missing SWC file param')
 
-        cell = h.Import3d_SWC_read()
+        cell = self.h.Import3d_SWC_read()
         try:
             cell.input(self.config['swc_path'])
         except:
             raise ValueError('Cant open SWC file at: ', self.config['swc_path'])
 
-        i3d = h.Import3d_GUI(cell, False)
+        i3d = self.h.Import3d_GUI(cell, False)
         i3d.instantiate(None)
 
     def add_stim(self):
@@ -80,9 +73,9 @@ class NeuronWrapper:
             id_ = stim['id']
             section_ = stim['section']
             type_ = stim['type']
-            h_ref = num_to_type(type_)
+            h_ref = num_to_type(type_, self.h)
 
-            new_stim = h.IClamp(h_ref[id_](section_))
+            new_stim = self.h.IClamp(h_ref[id_](section_))
             new_stim.delay = stim['delay']
             new_stim.dur = stim['duration']
             new_stim.amp = stim['amplitude']
@@ -91,7 +84,7 @@ class NeuronWrapper:
 
     def add_record(self):
         if 'record' not in self.config:
-            return
+            raise ValueError('Missing Recordings')
         recordings = {}
         for record_ in self.config['record']:
             id_ = record_['id']
@@ -100,11 +93,11 @@ class NeuronWrapper:
             recording_type_ = record_['record_type']
             recording_key_ = recording_key(recording_type_, type_, id_, section_)
 
-            h_ref = num_to_type(type_)
+            h_ref = num_to_type(type_, self.h)
             h_ref = h_ref[id_](section_)
             h_ref = recording_type_to_ref(type_=recording_type_, h_ref=h_ref)
 
-            vrec = h.Vector()
+            vrec = self.h.Vector()
             vrec.record(h_ref)
 
             recordings[recording_key_] = vrec
@@ -114,20 +107,19 @@ class NeuronWrapper:
         self.init_params(params)
         self.init_swc()
 
-        h.celsius = self.config['celsius']
+        self.h.celsius = self.config['celsius']
 
-        h.soma[0].insert('hh')  # insert a Hodgkin-Huxley channel
-        # h.dend_0[8].insert('hh')
+        self.h.soma[0].insert('hh')  # insert a Hodgkin-Huxley channel
 
         stims = self.add_stim()
         recordings = self.add_record()
 
-        trec = h.Vector()
-        trec.record(h._ref_t)  # record time variable
+        trec = self.h.Vector()
+        trec.record(self.h._ref_t)  # record time variable
 
-        h.finitialize(self.config['rest_volt'])
-        h.dt = self.config['dt']
-        neuron.run(self.config['sim_time'])
+        self.h.finitialize(self.config['rest_volt'])
+        self.h.dt = self.config['dt']
+        self.neuron_dy.run(self.config['sim_time'])
 
         result = {'time': np.array(trec).tolist()}
         for record_key in recordings:
@@ -137,31 +129,34 @@ class NeuronWrapper:
 
 
 if __name__ == "__main__":
-    wrap = NeuronWrapper()
+    wrap = NeuronWrapper('../app/src/share/config.json')
+
     stim = [{'id': 0, 'section': 0.5, 'type': 0, 'delay': 20, 'duration': 25, 'amplitude': 0.5},
             ]
     record = [{'id': 0, 'section': 0.5, 'type': 1, 'record_type': 'volt'},
               {'id': 0, 'section': 0.5, 'type': 0, 'record_type': 'volt'}
-            ]
-    res = wrap.run([{'id': 'swc_path', 'value': 'C:/Users/t-deangeckt/Downloads/swcTree.swc'},
-                    {'id': 'sim_time', 'value': 200},
-                    {'id': 'rest_volt', 'value': -65},
-                    {'id': 'stim', 'value': stim},
-                    {'id': 'record', 'value': record}])
+              ]
+
+    res = wrap.run(
+        [{'id': 'swc_path', 'value': 'C:/Users/t-deangeckt/Downloads/swcTree.swc'},
+         {'id': 'sim_time', 'value': 200},
+         {'id': 'rest_volt', 'value': -65},
+         {'id': 'stim', 'value': stim},
+         {'id': 'record', 'value': record}])
 
     subplot(1, 2, 1)
     r_key1 = recording_key('volt', 1, 0, 0.5)
     r_key2 = recording_key('volt', 0, 0, 0.5)
 
     plot(np.array(res['time']), np.array(res[r_key1]))
-    xlim((0, h.tstop))
+    xlim((0, 200))
     ylabel(r_key1)
     xlabel('Time (ms)')
     tight_layout()
 
     subplot(1, 2, 2)
     plot(np.array(res['time']), np.array(res[r_key2]))
-    xlim((0, h.tstop))
+    xlim((0, 200))
     xlabel('Time (ms)')
     ylabel(r_key2)
     show()
