9b54dfe03c07f772539ea7668f37b2a30e2952c6
Dean Geckt <t-deangeckt@microsoft.com>
2021-05-09 17:53:54 +0300

    some order

--- app/src/tree/TreeCanvasAnimated.tsx
+++ app/src/tree/TreeCanvasAnimated.tsx
@@ -1,46 +1,7 @@
-import React, { useContext, useEffect } from 'react';
-import { AppContext } from '../AppContext';
-import { getStage, RenderILine } from '../Wrapper';
-import { useSimulateCanvas } from './useSimulateCanvas';
+import React from 'react';
 import AnimatedLine from './AnimatedLine';
 
-function TreeCanvas() {
-    const { state, setState } = useContext(AppContext);
-    const { getLinesArrayNoRoot, updateTree } = useSimulateCanvas();
-
-    const widSize = window.document.getElementById('AnimCanvas')?.offsetWidth;
-    const [stageScale, setStageScale] = React.useState(1);
-    const [stageCoord, setStageCoord] = React.useState({ x: 0, y: 0 });
-
-    useEffect(() => {
-        if (widSize && widSize !== state.stage.width) {
-            console.log('changing stage size');
-            const newStage = getStage('AnimCanvas');
-            const sections = { ...state.sections };
-            updateTree(newStage.rootX - state.stage.rootX, newStage.rootY - state.stage.rootY);
-            setState({ ...state, sections: sections, stage: newStage });
-        }
-    }, [widSize, state]);
-
-    const handleWheel = (e: any) => {
-        e.evt.preventDefault();
-
-        const scaleBy = 1.1;
-        const stage = e.target.getStage();
-        const oldScale = stage.scaleX();
-        const mousePointTo = {
-            x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
-            y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale,
-        };
-
-        const newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
-        setStageCoord({
-            x: -(mousePointTo.x - stage.getPointerPosition().x / newScale) * newScale,
-            y: -(mousePointTo.y - stage.getPointerPosition().y / newScale) * newScale,
-        });
-        setStageScale(newScale);
-    };
-
+function TreeCanvasAnimated() {
     return (
         <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }} id={'AnimCanvas'}>
             <AnimatedLine />
@@ -48,4 +9,4 @@ function TreeCanvas() {
     );
 }
 
-export default TreeCanvas;
+export default TreeCanvasAnimated;
